9f4255737c7d0e986ebe0f04c90dbd78e005ee29
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2018-05-17 14:38:00 -0500

    Create TestCase.java

---
+++ TestCase.java
@@ -0,0 +1,12 @@
+package MadLibs;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestCase {
+
+	@Test
+	public void test() throws Exception{
+		fail("Not yet implemented");
+	}
+
+}
